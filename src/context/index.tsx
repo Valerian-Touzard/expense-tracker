e4ca1550a34bc26524054e398b329550e20ab8e1
vtouzard <vtouzard244@gmail.com>
2024-03-18 10:22:45 +0100

    feat: Ajout des informations dans notre context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,11 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const GlobalContext = createContext(null);
+export const GlobalContext = createContext({});
 
-const GlobalState = ({children}: React.PropsWithChildren) => {
+export type FormData = {
+  type: "expense" | "income";
+  amount: number;
+  description: string;
+};
+
+const GlobalState = ({ children }: React.PropsWithChildren) => {
+  const [formData, setFormData] = useState<FormData>({
+    type: "expense",
+    amount: 0,
+    description: "",
+  });
+
+  const [value, setValue] = useState("expense");
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [allTransaction, setAllTransaction] = useState<FormData[]>([]);
+
   return (
-    <GlobalContext.Provider value={null}>{children}</GlobalContext.Provider>
-  )
-}
+    <GlobalContext.Provider
+      value={{
+        value,
+        setValue,
+        totalExpense,
+        setTotalExpense,
+        totalIncome,
+        setTotalIncome,
+        allTransaction,
+        setAllTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
 
-export default GlobalState
+export default GlobalState;
