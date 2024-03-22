452220906a901e0ed9786be5086d0b1bc34c79fb
vtouzard <vtouzard244@gmail.com>
2024-03-22 09:31:59 +0100

    feat: Ajout de la méthode pour récupérer une transaction

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -23,6 +23,17 @@ class TransactionService {
       body: JSON.stringify(transaction),
     }).catch((err) => console.error(err));
   }
+
+  /**
+   * Permet de récupérer une transaction identifier par son id
+   * @param idTransaction string
+   * @returns La transaction identifier par son id
+   */
+  async getOneTransaction(idTransaction: string) {
+    return await fetch(`http://localhost:8080/transactions/${idTransaction}`)
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
+  }
 }
 
 export const transactionService = Object.freeze(new TransactionService());
